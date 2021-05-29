168c974e6f425cc61aee3468c8a038033342d516
Tayssir Hassan <tayssirhassan.th@gmail.com>
2021-05-29 13:14:40 +0200

    Charger Plots

--- data/out/engine/stop_charge/stop_charge.m
+++ data/out/engine/stop_charge/stop_charge.m
@@ -22,18 +22,27 @@ title('Stop 2','FontSize',24');
 
 figure(3);
 set(gca,'FontSize',24);
-plot(Stop_Charg_1_f63+(15*0.04),'Linewidth',4);
+plot(Stop_Charg_1_f63+(15*0.04),'o','Linewidth',0.5);
 hold on;
-plot(Stop_Charg_2_eef+(15*0.04),'Linewidth',4);
+plot(Stop_Charg_2_eef+(15*0.04),'o','Linewidth',0.5);
 hold on;
-plot(Stop_1_a54,'Linewidth',4);
+plot(Stop_1_a54,'o','Linewidth',4);
 hold on;
-plot(Stop_2_be8,'Linewidth',4);
+plot(Stop_2_be8,'o','Linewidth',4);
 hold on;
-plot(Stop_3_d88,'Linewidth',4);
+plot(Stop_3_d88,'o','Linewidth',4);
 hold on;
-plot(Stop_4_d36,'Linewidth',4);
+plot(Stop_4_d36,'o','Linewidth',4);
 hold on;
-plot(Stop_5_dc0,'Linewidth',4);
+plot(Stop_5_dc0,'o','Linewidth',4);
 hold on;
 grid();
+
+drop1 = (12.73 - 12.53)/12.73; %Stop_Charg_1_f63
+drop2 = (12.48 - 12.25)/12.48;
+drop3 = (13.43 - 13.1)/13.43;
+drop4 = (13.57 - 13.11)/13.57;
+drop5 = (13.85 - 13.39)/13.85;
+drop6 = (13.56 - 13.1)/13.56;
+drop7 = (13.71 - 13.37)/13.71;
+
